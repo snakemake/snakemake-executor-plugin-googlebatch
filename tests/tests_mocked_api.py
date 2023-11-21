ae44585b4383ccdc1f98c9693510ca341025d2e8
Johannes Koester <johannes.koester@uni-due.de>
2023-11-21 17:12:17 +0100

    fixes

--- tests/tests_mocked_api.py
+++ tests/tests_mocked_api.py
@@ -30,5 +30,11 @@ class TestWorkflowsMocked(TestWorkflowsBase):
         "snakemake_storage_plugin_s3.StorageObject.managed_size",
         new=AsyncMock(autospec=True, return_value=0),
     )
+    @patch(
+        # mocking has to happen in the importing module, see
+        # http://www.gregreda.com/2021/06/28/mocking-imported-module-function-python
+        "snakemake.jobs.wait_for_files",
+        new=AsyncMock(autospec=True),
+    )
     def run_workflow(self, *args, **kwargs):
         super().run_workflow(*args, **kwargs)
