4ee057c52a8d46b9d95ba551e527d27b5871c6e2
Johannes Koester <johannes.koester@uni-due.de>
2024-08-14 13:16:25 +0200

    fix type and apply todo

--- tests/__init__.py
+++ tests/__init__.py
@@ -30,9 +30,7 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase
     def get_remote_execution_settings(
         self,
     ) -> snakemake.settings.types.RemoteExecutionSettings:
-        return snakemake.settings.RemoteExecutionSettings(
+        return snakemake.settings.types.RemoteExecutionSettings(
             seconds_between_status_checks=10,
             envvars=self.get_envvars(),
-            # TODO: remove once we have switched to stable snakemake for dev
-            container_image="snakemake/snakemake:latest",
         )
